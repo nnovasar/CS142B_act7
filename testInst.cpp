1b8b9a9a4ef7b78de942663a3a2a1d0b3d707224
nnovasar <38850106+nnovasar@users.noreply.github.com>
2021-05-14 09:46:04 -0700

    Create testInst.cpp

---
+++ testInst.cpp
@@ -0,0 +1,34 @@
+// Look at the testInst.cpp file provided in the GEN repository and write Instructions for the following Mini-py functions.
+
+/* Part a
+
+def foo():
+    a: int = 1
+    b: int = a + 2
+*/
+
+
+/* Part b
+
+def abs(a: int)->int:
+    if a < 0:
+        return -a
+    return a
+*/
+
+
+/* Part c
+
+def F_to_C(int F)-> int:
+    return (F-32) * (5/9)
+*/
+
+
+/* Part d
+
+bool can_graduate(int gpa, int units) -> bool:
+    if gpa > 2 and units >= 180:
+        return True;
+    return False;
+*/
+
